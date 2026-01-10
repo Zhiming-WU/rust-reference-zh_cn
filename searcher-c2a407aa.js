c03d7820419250aefa6bfa0ee7040ba6bebdd047
ZhimingWu <zhiming.wu.atol@outlook.com>
2026-01-11 00:01:01 +0800

    Update for new translation

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-19dcf0ad.js',
+                path_to_root + 'searchindex-4a49bb37.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
